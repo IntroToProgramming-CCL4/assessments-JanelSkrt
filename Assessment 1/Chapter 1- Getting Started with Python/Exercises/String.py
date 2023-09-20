11fe721ab926080a3c80ca5d791b7d6374e89f93
JanelSkrt <144931141+JanelSkrt@users.noreply.github.com>
2023-09-21 00:09:13 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/String.py
@@ -0,0 +1,5 @@
+string1 = "Hello"
+string2 = "My name is"
+string3 = "Geof Janel Raneses"
+result_string = string1 + string2 + string3
+print(result_string)
---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Version of python.py
@@ -0,0 +1,4 @@
+import sys
+
+python_version = sys.version
+print("Python version", python_version)
