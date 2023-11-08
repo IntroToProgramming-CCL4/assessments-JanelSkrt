2198baf9c907709ce71c398eb0c60db506883ba3
JanelSkrt <144931141+JanelSkrt@users.noreply.github.com>
2023-11-08 19:29:52 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Ex3 Chapt3.py
@@ -0,0 +1,4 @@
+transpo = ['Honda Civic', 'Toyota Fortuner', 'Bentley Mini Cooper']
+print ('I would like to own a', transpo[0])
+print ('I like to ride my', transpo[2], 'to school')
+print ('I get home by riding my', transpo[1])
